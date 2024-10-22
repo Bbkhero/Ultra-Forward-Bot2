a8382b7049fede605cceb0a9d7495c93bdf96699
Bbkhero <pritamdebnath741223@gmail.com>
2024-10-22 16:48:15 +0530

    Update config.py

--- config.py
+++ config.py
@@ -11,7 +11,7 @@ import os
 class Config:
     API_ID = os.environ.get("API_ID", "26614080")
     API_HASH = os.environ.get("API_HASH", "7d2c9a5628814e1430b30a1f0dc0165b")
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "7594693248:AAHubu-qceibM25E03dicbwoxvvppEUWhYA") 
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "7475970261:AAEcQWN9J2d2M-SJRUeUEqpA6W597Sa6ezo") 
     BOT_SESSION = os.environ.get("BOT_SESSION", "forward-bot") 
     DB_URL = os.environ.get("DB_URL", "mongodb+srv://nsdh77ygz5:b0zdFJkd9frHADgT@cluster0.vvof2.mongodb.net/?retryWrites=true&w=majority")
     DB_NAME = os.environ.get("DB_NAME", "madflixbotz")
